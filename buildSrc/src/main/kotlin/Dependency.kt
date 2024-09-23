518a78a393d81e66b0aae1cbf5576bfac7d8f410
Jihyuk Lee <lead@sver.dev>
2024-09-23 20:46:12 +0900

    feat :: 필요없는 Dependency 주석 처리 / ProjectProperties 제거

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -1,41 +1,41 @@
 object Dependency {
-    object Gradle {
-        const val APPLICATION = "com.android.application"
-        const val LIBRARY = "com.android.library"
-        const val KOTLIN = "org.jetbrains.kotlin.android"
-        const val KSP = "com.google.devtools.ksp"
-    }
+//    object Gradle {
+//        const val APPLICATION = "com.android.application"
+//        const val LIBRARY = "com.android.library"
+//        const val KOTLIN = "org.jetbrains.kotlin.android"
+//        const val KSP = "com.google.devtools.ksp"
+//    }
+//
+//    object AndroidX {
+//        const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+//        const val LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE}"
+//        const val COMPOSE_LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-compose:${Versions.COMPOSE_LIFECYCLE}"
+//        const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
+//        const val SPLASH = "androidx.core:core-splashscreen:${Versions.SPLASH}"
+//    }
+//
+//    object Compose {
+//        const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
+//        const val COMPOSE = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+//        const val COMPOSE_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE}"
+//        const val COMPOSE_MATERIAL3 = "androidx.compose.material3:material3:${Versions.COMPOSE_MATERIAL3}"
+//        const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Versions.COMPOSE_MATERIAL}"
+//        const val COMPOSE_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+//        const val COMPOSE_GRAPHICS = "androidx.compose.ui:ui-graphics:${Versions.COMPOSE}"
+//        const val COMPOSE_BOM = "androidx.compose:compose-bom:${Versions.COMPOSE_BOM}"
+//        const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:${Versions.COMPOSE_NAVIGATION}"
+//        const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:${Versions.COMPOSE_FOUNDATION}"
+//        const val COMPOSE_IMMUTABLELIST = "org.jetbrains.kotlinx:kotlinx-collections-immutable:${Versions.COMPOSE_IMMUTABLELIST}"
+//    }
 
-    object AndroidX {
-        const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
-        const val LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE}"
-        const val COMPOSE_LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-compose:${Versions.COMPOSE_LIFECYCLE}"
-        const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
-        const val SPLASH = "androidx.core:core-splashscreen:${Versions.SPLASH}"
-    }
-
-    object Compose {
-        const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
-        const val COMPOSE = "androidx.compose.ui:ui:${Versions.COMPOSE}"
-        const val COMPOSE_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE}"
-        const val COMPOSE_MATERIAL3 = "androidx.compose.material3:material3:${Versions.COMPOSE_MATERIAL3}"
-        const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Versions.COMPOSE_MATERIAL}"
-        const val COMPOSE_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
-        const val COMPOSE_GRAPHICS = "androidx.compose.ui:ui-graphics:${Versions.COMPOSE}"
-        const val COMPOSE_BOM = "androidx.compose:compose-bom:${Versions.COMPOSE_BOM}"
-        const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:${Versions.COMPOSE_NAVIGATION}"
-        const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:${Versions.COMPOSE_FOUNDATION}"
-        const val COMPOSE_IMMUTABLELIST = "org.jetbrains.kotlinx:kotlinx-collections-immutable:${Versions.COMPOSE_IMMUTABLELIST}"
-    }
-
-    object Google {
-        const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
-
-        const val GOOGLE_SERVICES_PLUGIN = "com.google.gms.google-services"
-        // FIREBASE
-
-        const val SWIPE_REFRESH ="com.google.accompanist:accompanist-swiperefresh:${Versions.SWIPE_REFRESH}"
-    }
+//    object Google {
+//        const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+//
+//        const val GOOGLE_SERVICES_PLUGIN = "com.google.gms.google-services"
+//        // FIREBASE
+//
+//        const val SWIPE_REFRESH ="com.google.accompanist:accompanist-swiperefresh:${Versions.SWIPE_REFRESH}"
+//    }
 
     object Coil {
         const val COIL = "io.coil-kt:coil-compose:${Versions.COIL}"
@@ -59,17 +59,13 @@ object Dependency {
         const val HILT_NAVIGATION_COMPOSE = "androidx.hilt:hilt-navigation-compose:${Versions.HILT_NAVIGATION}"
     }
 
-    object Test {
-        const val JUNIT = "junit:junit:${Versions.JUNIT}"
-        const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
-        const val ESPRESSO = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
-        const val COMPOSE_JUNIT = "androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}"
-        const val COMPOSE_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Versions.COMPOSE}"
-    }
-
-    object Gauth {
-        const val GAUTH = "com.github.GSM-MSG:GAuth-Signin-Android:${Versions.GAUTH}"
-    }
+//    object Test {
+//        const val JUNIT = "junit:junit:${Versions.JUNIT}"
+//        const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+//        const val ESPRESSO = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
+//        const val COMPOSE_JUNIT = "androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}"
+//        const val COMPOSE_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Versions.COMPOSE}"
+//    }
 
     object DataStore {
         const val PREFERENCES = "androidx.datastore:datastore-preferences:${Versions.PREFERENCES}"
