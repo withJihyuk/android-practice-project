45f7fe2b0826b7c887722b1d58ef78b6b0e0226e
Jihyuk Lee <lead@sver.dev>
2024-09-24 09:17:45 +0900

    feat :: @Immutable 제거 및 val 로 변경

--- app/src/main/java/com/example/practice/dto/ConvertImage.kt
+++ app/src/main/java/com/example/practice/dto/ConvertImage.kt
@@ -1,8 +1,5 @@
 package com.example.practice.dto
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 data class ConvertImage(
-    var id: String
+    val id: String
 )
--- app/src/main/java/com/example/practice/dto/ResultImage.kt
+++ app/src/main/java/com/example/practice/dto/ResultImage.kt
@@ -1,19 +1,17 @@
 package com.example.practice.dto
 
-import androidx.compose.runtime.Immutable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.UUID
 
-@Immutable
 @Entity("resultImage")
 data class ResultImage(
     @PrimaryKey(autoGenerate = true)
-    var id: UUID,
-    var deviceId: String,
-    var bodyImageId: UUID,
-    var clothImageId: UUID,
-    var resultImageId: UUID,
-    var productName: String,
-    var productUrl: String
+    val id: UUID,
+    val deviceId: String,
+    val bodyImageId: UUID,
+    val clothImageId: UUID,
+    val resultImageId: UUID,
+    val productName: String,
+    val productUrl: String
 )
