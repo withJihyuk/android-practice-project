8bf950230c97651726679cd4903713d361f8ed62
Jihyuk Lee <lead@sver.dev>
2024-09-30 12:16:32 +0900

    feat :: buildSrc 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ configurations.implementation {
 }
 
 dependencies {
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/practice/ViewModel.kt
+++ app/src/main/java/com/example/practice/ViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.practice
 
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivityViewModel : ViewModel() {
+    var clickedCount: Int = 0
+
+    
+}
